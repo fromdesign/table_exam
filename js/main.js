c6368caf0ac11a42dcd74d87c613e11b34985782
DESKTOP-REFDUUO\Jeong <heyjessisexy@naver.com>
2018-11-14 18:40:29 +0900

    'initialize'

--- js/main.js
+++ js/main.js
@@ -1,6 +1,87 @@
-var tbodyEl = document.getElementsByTagName('tbody')[0];
-var store = '';
-for (var i = 0; i < data.length; i++) {
-	store += '<tr><td>' + data[i].index + '</td><td>' + data[i].name +'</td><td>' + data[i].location + '</td><td>' + data[i].eduCareer +'</td></tr>';
+//http://jsbin.com
+/*
+   Javascript
+*/
+
+// Type
+
+var store = ''; // 변수 (숫자, 문자열, Boolean, 함수, 배열, 객체, undefined, null, 변수);
+
+var string = '';
+
+var number1 = 10;
+
+var number2 = 100;
+
+var boolean = true;
+
+var Function = function () { number1 + number2 };
+
+var array = [1, 'Jeong', number1];
+
+var object = {
+	name : 'jeong',
+	job : 'publisher',
+	age : 24
 }
-tbodyEl.innerHTML = store;
+
+var temp1 = undefined;
+
+var temp2 = null;
+
+
+// Test 1
+
+var a = '자바스크립트가 ';
+var b = '너무 싫어요.';
+
+//console.log(a + b);
+
+
+// Test 2
+
+var a = '현재시간은 ';
+var b = 1;
+var c = 2;
+var d = '시 입니다.';
+
+//console.log(a + b + c + d);
+
+
+// Conditional statement
+
+/*
+연산자. +, -, *, /, %, ++, --, +=, -=, *=, /=, %=
+비교연산자. <, <=, >, >=, ===, ==, !==, !=
+논리연산자 &&, ||
+
+if (조건) {
+	조건이 참이면 실행 될 구문.
+} else {
+	조건이 참이 아니면 실행 될 구문.
+}
+
+exam
+
+if (number1 < number2) {
+	console.log(number1 + '은' + number2 + '보다 작다.');
+} else {
+	console.log(number1 + '은' + number2 + '보다 크다.');
+}
+
+*/
+
+// loop
+
+/*
+
+for (초기값, 조건, 증감 or 증가) {
+	반복할 일.
+}
+
+exam
+
+for (i = 0; i < 5; i++) {
+	console.log(i);
+}
+*/
